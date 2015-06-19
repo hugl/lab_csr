0cbcd2f14a7b170f957d1021c8480f7df5f7c45f
hugl <guilanhu@silkcloud.com>
2015-06-19 17:12:08 +0800

    update

---
+++ js/index.js
@@ -0,0 +1,10 @@
+/**
+ * Created by aaa on 15-6-19.
+ */
+var angular = require('angular');
+var app = angular.module('csr_chat',[]);
+app.controller('listControler',function($scope){
+    $scope.inReception = "接待";
+    $scope.myCustomers = "接待过的客户";
+    $scope.allCustomers = "所有客户";
+})
